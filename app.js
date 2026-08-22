cfdff892e6c15e8bb0829cb2ae3ad9cdecac4098
Tajim <com.tajim@gmail.com>
2026-08-22 11:13:00 +0600

    fix inline skeleton

--- app.js
+++ app.js
@@ -376,7 +376,9 @@ document.addEventListener('DOMContentLoaded', () => {
       if (!startTimestamp) startTimestamp = timestamp;
       const progress = Math.min((timestamp - startTimestamp) / duration, 1);
       const current = Math.floor(progress * (end - start) + start);
+      
       obj.innerHTML = current.toLocaleString();
+      
       if (progress < 1) {
         window.requestAnimationFrame(step);
       }
